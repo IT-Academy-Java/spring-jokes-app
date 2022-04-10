dd14b345ed5b1855cf16986fbaa76647499990c2
Elena Piaggio <elenapiaggio@gmail.com>
2022-04-10 17:45:26 +0200

    ADD joke controller

---
+++ src/main/java/com/competidor/jokesapp/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package com.competidor.jokesapp.controllers;
+
+import com.competidor.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+  private final JokeService jokeService;
+
+  public JokeController(JokeService jokeService) {
+    this.jokeService = jokeService;
+  }
+
+  @RequestMapping({"/", ""})
+  public String showJoke(Model model){
+    model.addAttribute("joke", jokeService.getJoke());
+    return "index";
+  }
+}
