bb894764cd916ea27642c5b72638e928715c246a
Elena Piaggio <elenapiaggio@gmail.com>
2022-04-10 17:46:54 +0200

    ADD joke controller. Closes #3

--- src/main/java/com/competidor/jokesapp/controllers/JokeController.java
+++ src/main/java/com/competidor/jokesapp/controllers/JokeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 public class JokeController {
 
